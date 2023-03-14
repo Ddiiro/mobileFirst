8d97972cd4c70b47288cdd123b970a27f901b6ba
Ddiiro <ddiiro92@gmail.com>
2023-03-14 13:03:51 +0300

    Finalise the pop up window section of the hamburger button-feature

--- navmodal.js
+++ navmodal.js
@@ -6,6 +6,11 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// Get the <a> element that closes the modal when the links are clicked
+var linkPortfolio = document.getElementsByClassName("link-x")[0];
+var linkAbout = document.getElementsByClassName("link-1")[0];
+var linkContact = document.getElementsByClassName("link-2")[0];
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -13,6 +18,21 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on porfolio link, close the modal
+linkPortfolio.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on about link, close the modal
+linkAbout.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on contact link, close the modal
+linkContact.onclick = function() {
   modal.style.display = "none";
 }
 
