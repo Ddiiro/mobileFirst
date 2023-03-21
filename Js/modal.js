61f8f58bdb4bde32b7bfbbdd22283c753b01b7bd
Ddiiro <ddiiro92@gmail.com>
2023-03-21 13:18:39 +0300

    Add the Modal pop up design with a grey background

--- Js/modal.js
+++ Js/modal.js
@@ -1,45 +1,45 @@
-const modal = document.getElementById('myModal');
+const modal1 = document.getElementById('myModal1');
 
-// Get the button that opens the modal
-const btn = document.getElementById('myBtn');
+// Get the button that opens the modal1
+const btn = document.getElementById('myBtn1');
 
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the modal1
 const span = document.getElementsByClassName('close')[0];
 
-// Get the <a> element that closes the modal when the links are clicked
+// Get the <a> element that closes the modal1 when the links are clicked
 const linkPortfolio = document.getElementsByClassName('link-x')[0];
 const linkAbout = document.getElementsByClassName('link-1')[0];
 const linkContact = document.getElementsByClassName('link-2')[0];
 
-// When the user clicks the button, open the modal
+// When the user clicks the button, open the modal1
 btn.onclick = function btn() {
-  modal.style.display = 'block';
-  modal.style.backgroundColor = 'none';
+  modal1.style.display = 'block';
+  modal1.style.backgroundColor = 'none';
 };
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modal1
 span.onclick = function span() {
-  modal.style.display = 'none';
+  modal1.style.display = 'none';
 };
 
-// When the user clicks on porfolio link, close the modal
+// When the user clicks on porfolio link, close the modal1
 linkPortfolio.onclick = function linkPortfolio() {
-  modal.style.display = 'none';
+  modal1.style.display = 'none';
 };
 
-// When the user clicks on about link, close the modal
+// When the user clicks on about link, close the modal1
 linkAbout.onclick = function linkAbout() {
-  modal.style.display = 'none';
+  modal1.style.display = 'none';
 };
 
-// When the user clicks on contact link, close the modal
+// When the user clicks on contact link, close the modal1
 linkContact.onclick = function linkContact() {
-  modal.style.display = 'none';
+  modal1.style.display = 'none';
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal1, close it
 window.onclick = function window(event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
+  if (event.target === modal1) {
+    modal1.style.display = 'none';
   }
 };
--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,9 @@
-const cards = [
+const Mobilecards = [
   {
     title: 'Tonic',
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: '/Images/Snapshoot Portfolio.png',
-    subtitle: [
-      {
-        stackName: 'CANOPY',
-      },
-      {
-        stack: 'Backend Dev',
-      },
-      {
-        stack: '2015',
-      },
-    ],
     technologies: [
       {
         name: 'Html',
@@ -32,17 +21,6 @@ const cards = [
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: './Images/Snapshoot Portfolio (2).png',
-    subtitle: [
-      {
-        stackName: 'CANOPY',
-      },
-      {
-        stack: 'Backend Dev',
-      },
-      {
-        stack: '2015',
-      },
-    ],
     technologies: [
       {
         name: 'Html',
@@ -60,17 +38,6 @@ const cards = [
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: './Images/Snapshoot Portfolio (1).png',
-    subtitle: [
-      {
-        stackName: 'CANOPY',
-      },
-      {
-        stack: 'Backend Dev',
-      },
-      {
-        stack: '2015',
-      },
-    ],
     technologies: [
       {
         name: 'Html',
@@ -88,17 +55,6 @@ const cards = [
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: './Images/Snapshoot Portfolio (3).png',
-    subtitle: [
-      {
-        stackName: 'CANOPY',
-      },
-      {
-        stack: 'Backend Dev',
-      },
-      {
-        stack: '2015',
-      },
-    ],
     technologies: [
       {
         name: 'Html',
@@ -113,9 +69,10 @@ const cards = [
   },
 ];
 
-cards.forEach((card) => {
+Mobilecards.forEach((card) => {
   const main = document.querySelector('#main');
   main.classList.add('grid-container');
+
   const gridItem = document.createElement('div');
   gridItem.classList.add('card');
   main.append(gridItem);
@@ -189,119 +146,18 @@ cards.forEach((card) => {
   const button = document.createElement('button');
   button.classList.add('button');
   gridItem.append(button);
-  const modal = document.createElement('div');
-  modal.classList.add('modal-Container');
-  gridItem.append(modal);
-  button.addEventListener('click', () => {
-    modal.style.display = 'block';
-  });
 
+  // Modal set up
   const anchorDetails = document.createElement('a');
   button.append(anchorDetails);
   anchorDetails.textContent = 'See Project';
-  const exit = document.createElement('div');
-  exit.classList.add('exit');
-  modal.append(exit);
-  // const closeButton = document.createElement('img');
-  // closeButton.classList.add('closepop');
-  // closeButton.alt = 'closepop img';
-  // closeButton.src = 'Images/cross.png';
-  // exit.append(closeButton);
-  // closeButton.addEventListener('click', () => {
-  //   modal.style.display = 'none';
-  // });
 
-  // const titlePop = document.createElement('h1');
-  // titlePop.classList.add('tonicPop');
-  // modal.append(titlePop);
-  // titlePop.innerText = card.title;
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.classList.add('hidden');
+  main.append(modal);
 
-  // const modalsubtext = document.createElement('div');
-  // modalsubtext.classList.add('modalsubtext');
-  // modal.append(modalsubtext);
-
-  // const modalsubcanopy = document.createElement('div');
-  // modalsubcanopy.classList.add('modalsubcanopy');
-  // modalsubtext.append(modalsubcanopy);
-
-  // const canopytext = document.createElement('p');
-  // canopytext.classList.add('canopytext');
-  // modalsubcanopy.append(canopytext);
-  // canopytext.innerText = 'CANOPY';
-
-  // const modaldetails = document.createElement('div');
-  // modaldetails.classList.add('modaldetails');
-  // modalsubtext.append(modaldetails);
-
-  // const backendtext = document.createElement('div');
-  // backendtext.classList.add('backendtext');
-  // modaldetails.append(backendtext);
-
-  // const backendp = document.createElement('div');
-  // backendtext.append(backendp);
-  // backendp.classList.add('backendp');
-
-  // const seperator = document.createElement('img');
-  // backendp.append(seperator);
-  // seperator.src = '/Images/Counter.png';
-  // seperator.alt = 'img';
-
-  // const p = document.createElement('p');
-  // backendp.append(p);
-  // backendp.classList.add('backendp');
-  // p.innerText = 'Backend';
-
-  // const seperato1 = document.createElement('img');
-  // backendp.append(seperato1);
-  // seperato1.classList.add('seperator');
-  // seperato1.src = '/Images/Counter.png';
-  // seperato1.alt = 'img';
-
-  // const p1 = document.createElement('p');
-  // backendp.append(p1);
-  // p1.classList.add('backendp-1');
-  // p1.innerText = '2015';
-
-  // const imgPop = document.createElement('img');
-  // imgPop.classList.add('snapPop');
-  // modal.append(imgPop);
-  // imgPop.src = card.image;
-  // imgPop.alt = 'img';
-
-  // const Lorem = document.createElement('p');
-  // imgPop.classList.add('Lorem');
-  // modal.append(Lorem);
-  // Lorem.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
-
-  // const modalButton = document.createElement('div');
-  // modalButton.classList.add('modalButton');
-  // modal.append(modalButton);
-
-  // const modalButton1 = document.createElement('button');
-  // modalButton1.classList.add('button');
-  // modalButton.append(modalButton1);
-
-  // const modalButton1a = document.createElement('a');
-  // modalButton1a.classList.add('a');
-  // modalButton1.append(modalButton1a);
-  // modalButton1a.innerText = 'See live';
-
-  // const modalButton1img = document.createElement('img');
-  // modalButton1img.classList.add('img');
-  // modalButton1a.append(modalButton1img);
-  // modalButton1img.src = 'See live';
-
-  // const modalButton2 = document.createElement('button');
-  // modalButton2.classList.add('button');
-  // modalButton.append(modalButton2);
-
-  // const modalButton2a = document.createElement('a');
-  // modalButton2a.classList.add('a');
-  // modalButton2.append(modalButton2a);
-  // modalButton2a.innerText = 'See live';
-
-  // const modalButton2img = document.createElement('img');
-  // modalButton2img.classList.add('img');
-  // modalButton2a.append(modalButton2img);
-  // modalButton2img.src = 'See live';
+  const h1 = document.createElement('h1');
+  modal.append(h1);
+  h1.innerHTML = 'hey';
 });
